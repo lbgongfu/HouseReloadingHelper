e67d92ce0f391db65026b84ef2f46d281a925c24
lbgongfu <lbgongfu@163.com>
2015-10-25 12:27:14 +0800

    init
    Initialize the program

---
+++ desktop/src/cn/lbgongfu/housereloadinghelper/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cn.lbgongfu.housereloadinghelper.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cn.lbgongfu.housereloadinghelper.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
